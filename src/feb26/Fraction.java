e36b9f981ccc2ba1bb79e7e7bf8cb32db6bc9e0f
Evan Smith <ejsmith2000@gmail.com>
2018-02-27 09:18:15 -0600

    made changes to Fraction and FractionRunner

--- src/feb26/Fraction.java
+++ src/feb26/Fraction.java
@@ -1,4 +1,27 @@
 package feb26;
 
-public class Fraction {
+public class Fraction implements Comparable<Fraction>
+{
+    private int num;
+    private int denom;
+
+    public Fraction(int num, int denom)
+    {
+        this.num=num;
+        this.denom=denom;
+    }
+
+    @Override
+    public int compareTo(Fraction o)
+    {
+        int difference = this.num*o.denom - o.num*this.denom;
+        if (difference > 0) return 1;
+        else if (difference < 0) return -1;
+        return 0;
+    }
+
+    private void simplify()
+    {
+
+    }
 }
---
+++ src/feb26/FractionRunner.java
@@ -0,0 +1,12 @@
+package feb26;
+
+public class FractionRunner
+{
+    public static void main(String[] args)
+    {
+        Fraction f1 = new Fraction( num 1, denom 4);
+        Fraction f2 = new Fraction(int num 1, int denom 4
+
+        )
+    }
+}
